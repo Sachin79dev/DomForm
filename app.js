f79b5c00ee9ef201f0acf1e10ba12dfdcddfe407
Sachin Singh <90830959+Sachin79dev@users.noreply.github.com>
2026-06-15 18:35:52 +0530

    Dom using Form
    Practicing Dom using form by taking name and email and on common image for user card and inheriting it in a row of cards.

---
+++ app.js
@@ -0,0 +1,29 @@
+const form = document.querySelector("form")
+const field1 = document.querySelector("#name")
+const field2 = document.querySelector("#email")
+const users = document.querySelector(".users")
+
+
+form.addEventListener('submit', (events) => {
+    events.preventDefault()
+
+    let name = field1.value
+    let email = field2.value
+
+    if(name.trim() === "" && email.trim() === "") return
+
+    users.innerHTML += `<div class="user_card">
+            <div class="img_box">
+                <img src="https://images.unsplash.com/photo-1464822759023-fed622ff2c3b?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="User Image">
+            </div>
+            
+            <div class="text">
+                <h3>${name}</h3>
+                <p>${email}</p>
+            </div>
+        </div>`
+
+
+    form.reset()
+
+})
